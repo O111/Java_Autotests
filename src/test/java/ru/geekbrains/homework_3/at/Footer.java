4488b3da6f41385b368a5770c493c880c4932e66
O111 <olegvyborov@gmail.com>
2020-05-24 12:03:25 +0300

    Homework_3_1

---
+++ src/test/java/ru/geekbrains/homework_3/at/Footer.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.homework_3.at;
+
+import org.openqa.selenium.By;
+
+public class Footer {
+
+    By entireFooterLocator = By.className("site-footer");
+    By vkLinkLocator = By.cssSelector("a[href='//vk.com/geekbrainsru']"); // остальные по аналогии
+    By feedbackLinkLocator = By.cssSelector("a[href='/feedbacks']");      // остальные по аналогии
+    By telLinkLocator = By.cssSelector("a[class='site-footer__phone']");
+}
