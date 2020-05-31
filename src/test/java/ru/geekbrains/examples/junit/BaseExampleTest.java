b0aa1765852bdf75af8048176453492d7616247c
O111 <olegvyborov@gmail.com>
2020-05-31 22:06:12 +0300

    Материал с урока

---
+++ src/test/java/ru/geekbrains/examples/junit/BaseExampleTest.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.examples.junit;
+
+import org.junit.jupiter.api.*;
+
+@Disabled
+public class BaseExampleTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Base class before all");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Base class before each");
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Base class after each");
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Base class after all");
+    }
+}
