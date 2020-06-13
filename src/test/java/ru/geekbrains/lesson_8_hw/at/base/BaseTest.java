e815743f3b2fcc74d322a868006f3c4d0ac41cae
O111 <olegvyborov@gmail.com>
2020-06-13 16:49:35 +0300

    First attempt to write and run Web UI Java autotests

---
+++ src/test/java/ru/geekbrains/lesson_8_hw/at/base/BaseTest.java
@@ -0,0 +1,36 @@
+package ru.geekbrains.lesson_8_hw.at.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest {
+    protected final String BASE_URL = "https://geekbrains.ru";
+    protected WebDriver driver;
+
+    @BeforeEach
+    @Step("Open Chrome browser")
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.setExperimentalOption("excludeSwitches",
+                Arrays.asList("disable-popup-blocking"));
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    @Step("Quit browser")
+    public void tearDown() {
+        driver.quit();
+    }
+}
